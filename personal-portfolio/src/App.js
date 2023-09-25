0be3b32df74c258ccb25a03552cf1b1fe73f753b
fizharoon <fizharoon@gmail.com>
2023-09-24 22:41:21 -0700

    Finished banner and skills section,started project

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './Components/NavBar';
+import {Banner} from './Components/Banner'
+import {Skills} from "./Components/Skills"
+import {Project} from "./Components/Project"
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Banner/>
+      <Skills/>
+      <Project/>
     </div>
   );
 }
